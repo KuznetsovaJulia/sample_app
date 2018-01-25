10b629e871a692d4b8a433fdb0814fdd33802c8a
KuznetsovaJulia <julia-kuz95@yandex.ru>
2018-01-25 12:29:26 +0300

    Finish

--- spec/features/title_spec.rb
+++ spec/features/title_spec.rb
@@ -4,19 +4,19 @@ feature 'when i visit page i want to see title' do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   scenario "Home title" do
-  visit '/static_pages/home'
+  visit '/home'
   expect(page).to have_title ("Home | #{@base_title}")
   end
   scenario "Contact title" do
-    visit '/static_pages/contact'
+    visit '/contact'
     expect(page).to have_title ("Contact | #{@base_title}")
   end
   scenario "Help title" do
-    visit '/static_pages/help'
+    visit '/help'
     expect(page).to have_title("Help | #{@base_title}")
   end
   scenario "About title" do
-    visit '/static_pages/about'
+    visit '/about'
     expect(page).to have_title ("About | #{@base_title}")
   end
   end
