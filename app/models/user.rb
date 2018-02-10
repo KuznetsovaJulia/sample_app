6ba739166da1636861a250ba2a6c2d2b0cd8707f
KuznetsovaJulia <julia-kuz95@yandex.ru>
2018-02-10 17:04:10 +0300

    chapter 8, commit 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,11 @@ class User < ApplicationRecord
               uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }
     has_secure_password
-
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
     FORBIDDEN_USERNAMES = %w{дура дебил}
     validate :name_is_allowed
     private
